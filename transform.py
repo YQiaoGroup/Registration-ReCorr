526a57c02167accad8cf3488d4b299297e0e56bd
TientJan <42082682+TientJan@users.noreply.github.com>
2025-11-15 13:15:12 +0800

    Create transform.py

---
+++ transform.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SpatialTransformer(nn.Module):
+
+    def __init__(self, mode='bilinear'):
+        super().__init__()
+        self.mode = mode
+
+    def forward(self, src, flow):
+        shape = flow.shape[2:]
+
+        vectors = [torch.arange(0, s) for s in shape]
+        grids = torch.meshgrid(vectors)
+        grid = torch.stack(grids)
+        grid = torch.unsqueeze(grid, 0)
+        grid = grid.type(torch.FloatTensor)
+        grid = grid.to(flow.device)
+
+        new_locs = grid + flow
+        for i in range(len(shape)):
+            new_locs[:, i, ...] = 2 * (new_locs[:, i, ...] / (shape[i] - 1) - 0.5)
+
+        new_locs = new_locs.permute(0, 2, 3, 4, 1)
+        new_locs = new_locs[..., [2, 1, 0]]
+
+        return F.grid_sample(src, new_locs, align_corners=True, mode=self.mode)
